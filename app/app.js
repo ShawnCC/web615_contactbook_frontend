e3c0ecf9579bb6efb1a7dce2cf0a8a46df34ff9d
Shawn Crusvar <scrusvar@alterisgroup.net>
2017-02-20 19:57:17 -0500

    - Week 8 in class complete

---
+++ app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Include any necessary libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Include root component
+import Index from './index';
+
+// Render our root component to the provided element
+ReactDOM.render(<Index/>, document.getElementById('app'));
